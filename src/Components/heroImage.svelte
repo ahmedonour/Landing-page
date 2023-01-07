cb2b9e2b5e891cb03384e85daec0ccfe048f4f51
Ahmed Onour <ahmedonour.49@gmail.com>
2023-01-07 22:08:37 +0200

    test

---
+++ src/Components/heroImage.svelte
@@ -0,0 +1,6 @@
+<div class="hero_Image">
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img src="image-hero-mobile.png" alt="image-hero-mobile">
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img src="image-hero-desktop.png" alt="image-hero-desktop">
+</div>
